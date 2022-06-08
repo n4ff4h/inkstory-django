f5afc2c59496b49ceabf235c2ffbc0c4dc93ed90
Naffah Abdulla Rasheed <mail@naffah.me>
2022-06-08 15:37:09 +0500

    feat: Style the pages using Bootstrap 5.2

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    title_tag = models.CharField(max_length=255, default="Title")
     # model.CASCADE: will delete all posts related to the user, if user is deleted
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
