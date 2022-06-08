c610d8ac88e47f3313c849c5573650a734118943
Naffah Abdulla Rasheed <mail@naffah.me>
2022-06-08 12:38:06 +0500

    feat: Populate posts in home and post-detail page

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .views import HomeView, PostDetailView
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', HomeView.as_view(), name="home"),
+    # pk: primary key of the model which is passed into the view
+    path('posts/<int:pk>', PostDetailView.as_view(), name="post-detail"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,16 @@
+# Note: ListView is used to get all the post records
+#       while DetailView is used to get just one record
+#       from the database.
+
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html', {})
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
