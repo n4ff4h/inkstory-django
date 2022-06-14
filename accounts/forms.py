64907a3fd533b06161002b4f958444e41e432d34
Naffah Abdulla Rasheed <mail@naffah.me>
2022-06-14 12:34:26 +0500

    feat: Add more fields and styling to register.html

---
+++ accounts/forms.py
@@ -0,0 +1,75 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(max_length=255,
+                                 required=True,
+                                 widget=forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 '
+                                                                        'text-gray-900 text-sm rounded-lg '
+                                                                        'focus:ring-blue-500 focus:border-blue-500 '
+                                                                        'block w-full p-2.5 dark:bg-gray-700 '
+                                                                        'dark:border-gray-600 '
+                                                                        'dark:placeholder-gray-400 dark:text-white '
+                                                                        'dark:focus:ring-blue-500 '
+                                                                        'dark:focus:border-blue-500'}))
+
+    last_name = forms.CharField(max_length=255,
+                                required=True,
+                                widget=forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 '
+                                                                       'text-gray-900 text-sm rounded-lg '
+                                                                       'focus:ring-blue-500 focus:border-blue-500 '
+                                                                       'block w-full p-2.5 dark:bg-gray-700 '
+                                                                       'dark:border-gray-600 '
+                                                                       'dark:placeholder-gray-400 dark:text-white '
+                                                                       'dark:focus:ring-blue-500 '
+                                                                       'dark:focus:border-blue-500'}))
+
+    username = forms.CharField(max_length=255,
+                               required=True,
+                               widget=forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 '
+                                                                      'text-gray-900 text-sm rounded-lg '
+                                                                      'focus:ring-blue-500 focus:border-blue-500 '
+                                                                      'block w-full p-2.5 dark:bg-gray-700 '
+                                                                      'dark:border-gray-600 '
+                                                                      'dark:placeholder-gray-400 dark:text-white '
+                                                                      'dark:focus:ring-blue-500 '
+                                                                      'dark:focus:border-blue-500'}))
+
+    email = forms.EmailField(max_length=255,
+                             required=True,
+                             widget=forms.EmailInput(attrs={'class': 'bg-gray-50 border border-gray-300 '
+                                                                     'text-gray-900 text-sm rounded-lg '
+                                                                     'focus:ring-blue-500 focus:border-blue-500 '
+                                                                     'block w-full p-2.5 dark:bg-gray-700 '
+                                                                     'dark:border-gray-600 '
+                                                                     'dark:placeholder-gray-400 dark:text-white '
+                                                                     'dark:focus:ring-blue-500 '
+                                                                     'dark:focus:border-blue-500'}))
+
+    password1 = forms.CharField(max_length=255,
+                                required=True,
+                                widget=forms.PasswordInput(attrs={'class': 'bg-gray-50 border border-gray-300 '
+                                                                           'text-gray-900 text-sm rounded-lg '
+                                                                           'focus:ring-blue-500 focus:border-blue-500 '
+                                                                           'block w-full p-2.5 dark:bg-gray-700 '
+                                                                           'dark:border-gray-600 '
+                                                                           'dark:placeholder-gray-400 dark:text-white '
+                                                                           'dark:focus:ring-blue-500 '
+                                                                           'dark:focus:border-blue-500'}))
+
+    password2 = forms.CharField(max_length=255,
+                                required=True,
+                                widget=forms.PasswordInput(attrs={'class': 'bg-gray-50 border border-gray-300 '
+                                                                           'text-gray-900 text-sm rounded-lg '
+                                                                           'focus:ring-blue-500 focus:border-blue-500 '
+                                                                           'block w-full p-2.5 dark:bg-gray-700 '
+                                                                           'dark:border-gray-600 '
+                                                                           'dark:placeholder-gray-400 dark:text-white '
+                                                                           'dark:focus:ring-blue-500 '
+                                                                           'dark:focus:border-blue-500'}))
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .forms import RegisterForm
+from django.contrib import messages
 
 
 # Create your views here.
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = RegisterForm
+    initial = {'key': 'value'}
     template_name = 'registration/register.html'
-    success_url = reverse_lazy('login')
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+
+            return redirect(to='login')
+
+        return render(request, self.template_name, {'form': form})
