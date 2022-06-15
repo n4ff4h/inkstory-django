09e719131b55892ccf8f18ca959f9832eee9c4f4
Naffah Abdulla Rasheed <mail@naffah.me>
2022-06-15 10:11:24 +0500

    feat: Change TinyMCE default config
    Ability to upload images

--- bloggist/settings.py
+++ bloggist/settings.py
@@ -153,3 +153,19 @@ LOGOUT_REDIRECT_URL = 'home'
 
 
 TAGGIT_CASE_INSENSITIVE = True
+
+
+TINYMCE_DEFAULT_CONFIG = {
+    "height": "400px",
+    "width": "100%",
+    "menubar": "file edit view insert format tools table help",
+    "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code "
+    "fullscreen insertdatetime media table paste code help wordcount spellchecker",
+    "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
+    "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
+    "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
+    "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
+    "a11ycheck ltr rtl | showcomments addcomment code",
+    "custom_undo_redo_levels": 10,
+    "language": "en_US",  # To force a specific language instead of the Django current language.
+}
