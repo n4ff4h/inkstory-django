2be79096e88b64fb0c6714d32e7111abc0632116
Naffah Abdulla Rasheed <mail@naffah.me>
2022-07-03 14:12:12 +0500

    refactor: Add custom colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,13 @@ module.exports = {
       center: true,
       padding: "1rem",
     },
+    colors: {
+      brand: "#3B82F6",
+      brand_darker: "#0B63F3",
+      dark: "#0F172A",
+      mid: "#CBD5E1",
+      light: "#F8FAFC",
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("flowbite/plugin")],
 };
